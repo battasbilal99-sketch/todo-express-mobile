f7c19fa836757160b444ee813834522032f14e52
battasbilal99-sketch <battasbilal99@gmail.com>
2025-08-24 04:06:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+// src/main.jsx
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
   </HashRouter>
