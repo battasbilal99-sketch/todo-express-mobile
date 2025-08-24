7c3f7eebe031bc94383b8111f6a2ec9ed4c36e40
battasbilal99-sketch <battasbilal99@gmail.com>
2025-08-24 03:52:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// GH Pages => sous-chemin /todo-express-mobile/
+// Android/localhost => racine "/"
+const isGhPages = process.env.VITE_GH_PAGES === 'true'
+
 export default defineConfig({
+  base: isGhPages ? '/todo-express-mobile/' : '/',
   plugins: [react()],
 })
